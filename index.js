4116731dd61031b70023937119dc88cce97771fd
Haider Ali Mohammed Ali Shaikh <Haider.Shaikh@coforge.com>
2022-12-31 22:47:50 +0530

    database setup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 var http=require('http');
+
 const express = require('express');
 const app = express();
 const userRoutes = require('./route/UserRoutes');
@@ -6,12 +8,5 @@ app.use(express.json());
 
 app.use(userRoutes);
 
-
 const server = http.createServer(app);
 server.listen(8000);
-
-
-// http.createServer((req,res)=>{
-//     res.writeHead(200,{'Content-Type':'text/html'});
-//     res.write('<h1>Hello Nodejs App!</h1>');
-// }).listen(8000)
